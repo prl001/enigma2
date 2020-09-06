b38858baadb2b3a76a9b89abb2a1dfc94d9f38c3
Huevos <rowland@moralet.com>
2020-09-06 12:22:13 +0200

    [Menu.py] Add mandatory widget "menu"

--- lib/python/Screens/Menu.py
+++ lib/python/Screens/Menu.py
@@ -205,7 +205,7 @@ class Menu(Screen, HelpableScreen, ProtectedScreen):
 
 
 	def __init__(self, session, parent):
-		Screen.__init__(self, session)
+		Screen.__init__(self, session, mandatoryWidgets=["menu"])
 		HelpableScreen.__init__(self)
 		list = []
 
