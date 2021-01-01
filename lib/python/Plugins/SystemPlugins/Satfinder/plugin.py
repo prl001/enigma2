c8e480ff851d705733ba439828d7f175d5df5174
Huevos <rowland@moralet.com>
2021-01-01 11:29:31 +0100

    Revert "[Satfinder] Tweak imports"
    This reverts commit 730cc26fc6745a2d352eb57b535040cec3ed60e7.

--- lib/python/Plugins/SystemPlugins/Satfinder/plugin.py
+++ lib/python/Plugins/SystemPlugins/Satfinder/plugin.py
@@ -18,22 +18,16 @@ from Screens.Screen import Screen # for services found class
 
 try: # for reading the current transport stream (SatfinderExtra)
 	from Plugins.SystemPlugins.AutoBouquetsMaker.scanner import dvbreader
-	dvbreader_available = True
-except ImportError as err:
-	print("[Satfinder] ImportError: '%s'" % (err))
-	dvbreader_available = False
-
-if dvbreader_available:
 	from Components.Sources.StaticText import StaticText
 	from Components.ScrollLabel import ScrollLabel
 	from Components.Label import Label
 	import time
 	import datetime
-	try:
-		import thread
-	except ImportError:
-		import _thread as thread
-
+	import thread
+	dvbreader_available = True
+except ImportError:
+	print "[Satfinder] import dvbreader not available"
+	dvbreader_available = False
 
 class Satfinder(ScanSetup, ServiceScan):
 	"""Inherits StaticText [key_red] and [key_green] properties from ScanSetup"""
